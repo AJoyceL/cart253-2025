c38a383f239e61f563932ecad29a5c1e941d235e
AJoyceL <LamAJoyc3@gmail.com>
2025-09-16 10:44:38 -0400

    Used variables to make the circle change colourschange

--- variables/exercise/js/script.js
+++ variables/exercise/js/script.js
@@ -11,13 +11,28 @@
  * create a canvas
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a circle in the center of the canvas
 */
 function draw() {
+    background(0);
 
+    //Draw the circle
+    push();
+    fill(153, 153, 255);
+    noStroke();
+    ellipse(width/2, height/2, 100, 100);
+    pop();
+
+    /* 
+   *Another option of colour with the variable *of mouseX and mouseY
+   *
+   * fill(mouseX, mouseY, 0);
+   * ellipse(mouseX, mouseY, 100, 100); 
+   *ellipse(width/2, height/2, mouseX, mouseY);
+   */
 }
