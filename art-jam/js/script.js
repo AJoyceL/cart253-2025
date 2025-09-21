ed18f5c0fe07ddc676ed0c9c93eac93adb7f48db
AJoyceL <LamAJoyc3@gmail.com>
2025-09-21 19:50:57 -0400

    Added the base shape of the face to the project using vertex from "introduction challenge" with partners.
    - The vertex was taken from the p5 references.

--- art-jam/js/script.js
+++ art-jam/js/script.js
@@ -2,9 +2,18 @@
  * Art Jam: Sefl Portrait
  * Joyce Angelina Lam
  * 
+ * Taking inspiration from the works of Pablo Picasso and cubism/abstract to create a self portrait.
  */
 
 "use strict";
+
+// // Variables for the face
+// let face = {
+//     x: 300,
+//     y: 450,
+//     width: 200,
+//     height: 300,
+// };
 
 /**
  * Creates the canvas
@@ -20,4 +29,27 @@ function setup() {
 function draw() {
     // Lavender blue background
     background(191, 179, 255);
+
+    drawFace();
+}
+
+function drawFace() {
+    push();
+    fill(255, 204, 153);
+    noStroke();
+
+    //Vertex declaratiuons in counter-clockwise order
+    beginShape();
+    vertex(450, 100);
+    vertex(400, 100);
+    vertex(200, 700);
+    vertex(400, 700);
+    vertex(400, 800);
+    vertex(300, 900);
+    vertex(600, 900);
+    vertex(500, 800);
+    endShape(CLOSE);
+    pop();
+
+   
 }
