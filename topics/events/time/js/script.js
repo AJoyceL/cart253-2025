5a6760e2883fc86ef78085a259544a14c2ce5045
AJoyceL <LamAJoyc3@gmail.com>
2025-09-30 10:05:24 -0400

    Created a time example

--- topics/events/time/js/script.js
+++ topics/events/time/js/script.js
@@ -1,18 +1,22 @@
 /**
- * Title of Project
+ * Time
  * Author Name
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
  */
 
 "use strict";
 
+const ball = {
+    x : 0, 
+    y: 200,
+    size: 50
+};
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+    * Creates the canvas
 */
 function setup() {
-
+    createCanvas(400, 400);
 }
 
 
@@ -20,5 +24,12 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
 
+    ball.x += 1;
+
+    Push();
+    noStroke();
+    ellipse(ball.x, ball.y, ball.size);
+    Pop();
 }
