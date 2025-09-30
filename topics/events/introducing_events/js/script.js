27da20bcd5f21bf8835230e86a4f45e4a72c64ca
AJoyceL <LamAJoyc3@gmail.com>
2025-09-30 10:13:31 -0400

    Made an event example

--- topics/events/introducing_events/js/script.js
+++ topics/events/introducing_events/js/script.js
@@ -7,16 +7,28 @@
 "use strict";
 
 /**
- * 
+ * creates a canvas, makes the canvas black
 */
 function setup() {
-
+    createCanvas(400, 400);
+    background(0);
 }
 
 
 /**
- * 
+ * does nothing!
 */
 function draw() {
 
+}
+
+/** 
+ * draws a yellow circle where the mouse is pressed
+*/
+function mousePressed() {
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(mouseX, mouseY, 50);
+    pop();
 }
