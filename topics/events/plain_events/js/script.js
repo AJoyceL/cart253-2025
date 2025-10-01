44af44df6cf9514d48356bf11108aad42be0533e
AJoyceL <LamAJoyc3@gmail.com>
2025-10-01 10:16:03 -0400

    Started the Plain JS events

---
+++ topics/events/plain_events/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Plain events
+ * Joyce Angelina Lam
+ * 
+ * experiemnting with event handling in Plain JS
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
