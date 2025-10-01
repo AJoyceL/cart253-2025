c47aa1edb398a6f08e69401065129f11f3db8544
AJoyceL <LamAJoyc3@gmail.com>
2025-10-01 10:29:14 -0400

    Added a keydown event handler for the background changing

--- topics/events/plain_events/js/script.js
+++ topics/events/plain_events/js/script.js
@@ -3,15 +3,32 @@
  * Joyce Angelina Lam
  * 
  * experiemnting with event handling in Plain JS
+ * 
+ * INFO: USE MOZILLA DEVELOPER NETWORK FOR REFERENCE
+ * https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/  addEventListener
  */
 
 "use strict";
+
+//Information about the current and posssible background fills
+const bg = {
+    fill: "#000000",
+    fills: {
+        black: "#000000",
+        white: "#FFFFFF",
+    },
+
+   switchKey: 32 //Spacebar
+}
 
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(400, 400);
 
+    //Listen foir keypresses
+    window.addEventListener("keydown",changeBG);
 }
 
 
@@ -19,5 +36,21 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+    background(bg.fill);
 }
+
+function changeBG(event) {
+    //Check if the key pressed is the spacebar
+    if(event.keyCode === bg.switchKey) {
+        if(bg.fill === bg.fills.black) {
+            bg.fill = bg.fills.white;
+        } 
+        else {
+            bg.fill = bg.fills.black;
+        }
+    }
+}
+
+
+//"mousedown" "mouseup" "mousemove" "mouseenter" "mouseleave" "dblclcick"
+//"keydown" "keyup" "keypress"
