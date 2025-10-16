4a76c538d902f9b7074501a7cd6c7a8d1426f995
AJoyceL <LamAJoyc3@gmail.com>
2025-10-16 12:55:56 -0400

    Wrote the basic for my TO DO list, subject to change in the future

--- mod_jam/js/script.js
+++ mod_jam/js/script.js
@@ -2,8 +2,32 @@
  * Mod Jam
  * Joyce Angelina Lam
  * 
- * So far I am unusure of the project's main objective, 
- * but I'll wait for Pippin's explenation.
+ * This is my mod jam submission!
+ * 
+ * TO DO:
+ * - TITLE SCREEN: title + instructions > start with spacebar/activating by catching your first fly
+ * 
+ * - GAME OVER SCREEN: timer runs out and didn't catch enough flies
+ * 
+ * - WIN SCREEN: congratulations for completing all levels
+ * 
+ * - TIMER: countdown timer for limiting the game time
+ * 
+ *  - SCORING SYSTEM: points for catching flies, points deducted for hitting enemies
+ * > ENEMIES: flies that deduct points when hit
+ * 
+ * - NEW MOVEMENT: frog moves by keyboard (WASD or arrow keys) + flies moves randomly (look up different movement types)
+ * 
+ * - NEW AUDIO/VISUAL EFFECTS: new sound effects for catching flies, hitting enemies, launching tongue, background music
+ * 
+ * POSSIBLY:
+ *  * - multiple levels: each level has a   different goal and time limit (if possible + different backgrounds and music)
+ * - health system: lose health when you hit an enemy, gain health when you catch a fly?
+ *  * - different types of flies: some are worth more points, some move faster, some move in different patterns
+ * - different types of enemies: some take more than one hit to defeat, some move faster, some move in different patterns
+ *
+ * Made with p5
+ * https://p5js.org/
  */
 
 "use strict";
