b75613f86734d278b9261b597093138aad59fb30
AJoyceL <Lamajoyc3@gmail.com>
2025-10-27 14:58:25 -0400

    finished gradient

--- topics/loops/gradiant/js/script.js
+++ topics/loops/gradiant/js/script.js
@@ -9,16 +9,30 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * create canvas
 */
 function setup() {
-
+    createCanvas(600, 300);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * draws gradient
 */
 function draw() {
+    background(0);
 
+    let y = 0;
+
+    for (let x = 0; x <= width; x+= 2){
+        const shade = map(x, 0, width, 0, 255);
+
+        push();
+        stroke(shade);
+        line(x, y, x, height);
+        pop();
+
+        
+        y += random(0, 2);
+    }
 }
