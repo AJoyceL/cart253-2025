f7c262e8a93f469c60d008879cb4e295e3435c87
AJoyceL <Lamajoyc3@gmail.com>
2025-10-31 12:44:22 -0400

    finished speech! speech!

--- topics/arrays/sequential-arrays/js/script.js
+++ topics/arrays/sequential-arrays/js/script.js
@@ -1,17 +1,22 @@
 /**
- * Title of Project
+ * Speech! Speech!
  * Author Name
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * interactive speech-playing interface!
  */
 
 "use strict";
 
+//  The speech itself
+let speech = ["Veni.", "Vidi.", "Vici.","Sensi malum."];
+// Which sentence in speech to display
+let speechIndex = 0;
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create canvas
 */
 function setup() {
+    createCanvas(600, 100);
 
 }
 
@@ -20,5 +25,29 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
 
+    // Get the current line of speech
+    let currentLine = speech[speechIndex];
+
+    // Display the line
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(currentLine, width/2, height/2);
+    pop();
+}
+
+/**
+ * Advances the speech
+ */
+function mousePressed(){
+    // Next line
+    speechIndex = speechIndex +1;
+
+    if (speechIndex >= speech.length){
+        // sytart over
+        speechIndex = 0;
+    }
 }
