f62ed5713312c36a9c1c076689234a922c52be93
AJoyceL <Lamajoyc3@gmail.com>
2025-11-08 13:57:01 -0500

    finished tarot project

--- topics/data/tarot/js/script.js
+++ topics/data/tarot/js/script.js
@@ -7,17 +7,53 @@
 
 "use strict";
 
+// our tarot data
+let tarot = undefined;
+
+// our fortune
+let fortune = "click to show a fortune";
+
+// Load tarot data
+function preload() {
+    tarot = loadJSON("assets/data/tarot_interpretations.json");
+}
+
 /**
- * tbd.
+ * display canvas.
 */
 function setup() {
-
+    createCanvas(800, 400);
 }
 
 
 /**
- * tbd.
+ * display tarot.
 */
 function draw() {
+    background(0);
 
+    // const card = tarot.tarot_interpretations[0];
+    // const meanings = card.meanings;
+    // const shadows = meanings.shadow;
+    // const fool = shadows[0];
+
+    //or a more direct way 
+
+    const fool = tarot.tarot_interpretations[0].meanings.shadow[0];
+
+    //display the info
+    push();
+    textSize(16);
+    fill("yellow");
+    textAlign(CENTER, CENTER);
+    text(fortune, width/2, height/2);
+    pop();
+}
+
+function mousePressed(){
+    // choose a random card
+    const card = random(tarot.tarot_interpretations);
+
+    //choose a random fortune
+    fortune = random(card.fortune_telling);
 }
